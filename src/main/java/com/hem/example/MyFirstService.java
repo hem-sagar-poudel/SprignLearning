91c57af5995f9eefb3133802de084723041f398e
hem-sagar-poudel <sagarpoudel17@gmail.com>
2025-12-15 17:42:57 +0545

    bug fix

--- src/main/java/com/hem/example/MyFirstService.java
+++ src/main/java/com/hem/example/MyFirstService.java
@@ -2,6 +2,7 @@ package com.hem.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.hem.example.Test.MyFirstClass;
@@ -10,8 +11,16 @@ import com.hem.example.Test.MyFirstClass;
 public class MyFirstService {
 
   private MyFirstClass myFirstClass;
+  private Environment environment;
 
-  // setter injection
+ 
+
+ 
+
+
+
+
+   // setter injection
   @Autowired
  // @Qualifier("bean1") can be here also
   public void setMyFirstClass( @Qualifier("bean1") MyFirstClass myFirstClass){
@@ -20,5 +29,26 @@ public class MyFirstService {
 
   public String tellAStory(){
     return "The dependency is saying : " + myFirstClass.sayHello();
+  }
+  
+
+
+
+  @Autowired
+  public void setEnvironment(Environment environment){
+    this.environment = environment;
+  }
+
+  public String getJavaVersion(){
+    return environment.getProperty("java.version");
+  }
+
+  public String getOsName(){
+    return environment.getProperty("os.name");
+  }
+
+  // accessed form application.properties
+  public String getProperties(){
+    return environment.getProperty("my.custom.properties");
   }
 }
