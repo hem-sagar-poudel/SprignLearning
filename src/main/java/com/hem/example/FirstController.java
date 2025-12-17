394476c81ce94701e656dd2321e8271c99f039ee
hem-sagar-poudel <sagarpoudel17@gmail.com>
2025-12-17 16:48:24 +0545

    first

--- src/main/java/com/hem/example/FirstController.java
+++ src/main/java/com/hem/example/FirstController.java
@@ -24,37 +24,4 @@ public class FirstController {
       return "Request Accepted " + message ;
   }
 
-  @PostMapping("/post-order")
-  // this is using POJOS // plane old java objects
-  public String postOrder(@RequestBody Order order) {
-      return "Request Accepted and order is " + order.toString(); // object order is converted to string
-  }
-
-  @PostMapping("/post-order-record")
-  // DTOS // Data Transfer objects // Java Records introduced in 14 and finalized in 16
-  // Record class is immutable // carry fixed set of values
-  // all fields are automatically final
-  public String postOrderRecord(@RequestBody OrderRecord orderRecord) {
-    return "Request Accepted and order is " + orderRecord.toString(); // object order is converted to string
-  }
-
-  //localhost:8080/hello/john
-  @GetMapping("/hello/{user-name}") //or make same path variable in all
-  public String pathVar(@PathVariable("user-name") String userName ) {
-      return "my value = " + userName;
-  }
-
-  //localhost:8080/hello/john
-  @GetMapping("/hello/{userName}/path") //or make same path variable in all
-  public String pathVarTest(@PathVariable String userName ) {
-      return "my value = " + userName;
-  }
-
-
-  //localhost:8080/hello/?name=john
-  @GetMapping("/hello/test") //or make same path variable in all
-  public String pathVarTests(@RequestParam("name") String name, @RequestParam("token") String token ) {
-      return "my value = " + name + " " + token;
-  }
-
 }
--- src/main/java/com/hem/example/OrderRecord.java
+++
@@ -1,9 +0,0 @@
-package com.hem.example;
-
-public record OrderRecord(
-  String customerName,
-  String  productName,
-  int quantity
-) {
-  
-}
