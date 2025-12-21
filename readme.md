2687a5046695046acce520e9445cd58b72cae06d
hem-sagar-poudel <sagarpoudel17@gmail.com>
2025-12-21 21:33:34 +0545

    crete entities

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 ## jdbc
 
 # java database connectivity
+
+application properties
+
+spring.jap.database.platform:org.hibernate.dialect.PostgresSqlDialect
+spring.datasource:driver-class-name:org.postgresql.Driver
---
+++ src/main/java/com/hem/example/Student.java
@@ -0,0 +1,88 @@
+package com.hem.example;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="T_STUDENT")
+public class Student {
+  @Id
+  @GeneratedValue
+  private Integer id;
+  @Column(name="c_fname",
+    length = 20
+  )
+  private String firstname;
+  private String lastname;
+  @Column(unique = true)
+  private String email;
+  private int age;
+  @Column(
+    updatable = false,
+    insertable =  true
+  )
+  private String updated_at;
+
+  public Student(){}
+
+
+  public Student(String firstname, String lastname, String email, int age, String updated_at){
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.age = age;
+    this.updated_at = updated_at;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getUpdated_at() {
+    return updated_at;
+  }
+
+  public void setUpdatedAt(String updated_at) {
+    this.updated_at = updated_at;
+  }
+
+}
